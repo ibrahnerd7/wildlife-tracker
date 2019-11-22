3fb5e39eea8573c424936fad7afd68dbbbf43707
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-11-22 12:44:59 +0300

    [fixes #169911116] Test that Sighting instatitates with animalId

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -3,8 +3,14 @@ package models;
 import java.sql.Timestamp;
 
 public class Sighting {
+    private int wildlifeId;
 
-    public Sighting(int animalId, String sightingZone, String rangerName, Timestamp sightingTime) {
+    public Sighting(int wildlifeId, String sightingZone, String rangerName, Timestamp sightingTime) {
+        this.wildlifeId = wildlifeId;
 
+    }
+
+    public int getWildlifeId() {
+        return wildlifeId;
     }
 }
--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -17,11 +17,20 @@ public class SightingTest {
 
     }
 
+    @Test
+    public void SightingInstantiatesWithWildlifeId_True(){
+        Sighting testSighting=setUpNewSighting();
+        int testWildlifeId=1;
+        assertEquals(testWildlifeId,testSighting.getWildlifeId());
+
+    }
+
     private Sighting setUpNewSighting() {
         Timestamp timestamp = new Timestamp(new Date().getTime());
         return new Sighting(1, "Zone A", "John Doe", timestamp);
 
     }
+
 
 
 
