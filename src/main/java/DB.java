189cf252779bdd1c24c94f52325a57e0c9902076
ibrahnerd7 <ibrahmaina7@gmail.com>
2019-11-25 00:57:40 +0300

    [fixes #169911201] Add heroku postgres configs

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,34 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "Access");
+    private static URI dbUri;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    public static Sql2o sql2o;
+
+    static {
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "moringa" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "Access" : dbUri.getUserInfo().split(":")[1];
+
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+
+        } catch (URISyntaxException e) {
+            logger.error("Unable to connect to the database.");
+        }
+
+    }
 }
